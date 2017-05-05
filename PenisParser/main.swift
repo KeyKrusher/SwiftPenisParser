88496e524afe1a35d220886e9718111205d26bf8
Jack Clark <chubz@Jacks-Mac-mini.local>
2017-05-04 22:03:17 -0500

    Initial Comment

---
+++ PenisParser/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PenisParser
+//
+//  Created by Jack Clark on 5/4/17.
+//  Copyright © 2017 Jack Clark. All rights reserved.
+//
+// http://stackoverflow.com/questions/24097826/read-and-write-data-from-text-file
+
+import Foundation
+
